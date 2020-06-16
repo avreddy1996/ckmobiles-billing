32458ab1fb9a9881bd0b3a425b995d8ae511a254
avreddy1996 <avreddy1996@gmail.com>
2020-06-17 00:31:17 +0530

    Updates

---
+++ precache-manifest.61fb6ce40cdf3abe38bf8271b8f8150f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb39c15c5279159512e381a748a4f2f",
+    "url": "/ckmobiles-billing/index.html"
+  },
+  {
+    "revision": "ec2b40d5fa984080dee4",
+    "url": "/ckmobiles-billing/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "b30fe380c96829dad48f",
+    "url": "/ckmobiles-billing/static/css/main.ab3d5471.chunk.css"
+  },
+  {
+    "revision": "ec2b40d5fa984080dee4",
+    "url": "/ckmobiles-billing/static/js/2.7ed04510.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/ckmobiles-billing/static/js/2.7ed04510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30fe380c96829dad48f",
+    "url": "/ckmobiles-billing/static/js/main.5e33ba9c.chunk.js"
+  },
+  {
+    "revision": "3df5101d0842d3bf1590",
+    "url": "/ckmobiles-billing/static/js/runtime-main.e9128a36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ckmobiles-billing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f17186cf8111f34ff68cdcdd72665d0e",
+    "url": "/ckmobiles-billing/static/media/logo.f17186cf.jpg"
+  },
+  {
+    "revision": "994ae77adcb99240e6918aa2f0dfe667",
+    "url": "/ckmobiles-billing/static/media/sign.994ae77a.png"
+  }
+]);
